fc36a0672cf4367ded78298cbe9e462065e2ebab
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-03-31 18:02:41 +0200

    still working on search

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -9,5 +9,18 @@ export default {
     },
 }
 </script>
-<template></template>
-<style scoped lang="scss"></style>
+<template>
+    <div class="text-center w-100 p-4">
+        <h3>Search your Monster-Type</h3>
+        <select>
+            <option value="All">All</option>
+            <option v-for="(type, index) in store.archetypeListApi" :value="type" :key="index">{{
+                type }}</option>
+        </select>
+    </div>
+</template>
+<style scoped lang="scss">
+select {
+    width: 10%;
+}
+</style>
