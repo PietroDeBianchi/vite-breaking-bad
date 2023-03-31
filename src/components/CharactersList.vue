b38f57e22397c4bee1ff3427ed0b4080c5a161ab
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-03-31 17:44:16 +0200

    working on search

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -18,11 +18,17 @@ export default {
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-3 " v-for="(card, index) in store.cardListApi" :key="index">
+            <div class="col-3 mb-2" v-for="(card, index) in store.cardListApi" :key="index">
                 <CharacterCard :image="card.card_images[0].image_url" :name="card.name" :archetype="card.archetype" />
             </div>
         </div>
     </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.card {
+    background-color: #CA9249;
+
+
+}
+</style>
